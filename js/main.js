b0936eaeb0e9a9fb4f4ece9d0ae97e673726ba25
Leon Guilherme <leonlopes1106@gmail.com>
2022-04-18 21:59:19 -0300

    criado JS , criando evento de click para os quadrados, aletarando entre x e o , definindo qual o jogador da vez...

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const quadradosVelha = document.querySelectorAll('.qd-velha');
+var jogador = "X";
+const jogadorSelecionado = document.querySelector('.vez-jogar p');
+
+quadradosVelha.forEach(quadrado => {
+    quadrado.addEventListener('click', e =>{
+        let clicandoQuadrado = e.target
+        if(clicandoQuadrado.innerHTML !== '-') return
+        
+        clicandoQuadrado.innerHTML = jogador;
+        clicandoQuadrado.style.color = '#F2D338';
+        
+        if(jogador === 'X'){
+            jogador = 'O'
+        }else{
+            jogador = 'X'
+        }
+
+        mudarJogador(jogador);
+        checaJogador()
+    });
+})
+
+function mudarJogador(valor){
+    jogador = valor
+    jogadorSelecionado.innerHTML = 'jogador: ' + jogador;
+}
+
+function checaJogador(){
+    
+
+    
+}
