bc7564a934903ce5da4ae4e5c79360e67ba0fea6
Leon Guilherme <leonlopes1106@gmail.com>
2022-04-21 11:08:57 -0300

    refatorando função do click

--- js/main.js
+++ js/main.js
@@ -2,32 +2,30 @@ const quadradosVelha = document.querySelectorAll('.qd-velha');
 var jogador = "X";
 const jogadorSelecionado = document.querySelector('.vez-jogar p');
 
-quadradosVelha.forEach(quadrado => {
-    quadrado.addEventListener('click', e =>{
-        let clicandoQuadrado = e.target
-        if(clicandoQuadrado.innerHTML !== '') return
-        
-        clicandoQuadrado.innerHTML = jogador;
-        clicandoQuadrado.style.color = '#F2D338';
-        
-        if(jogador === 'X'){
-            jogador = 'O'
-        }else{
-            jogador = 'X'
-        }
+const combinacoesVitorias = [[0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]];
 
-        mudarJogador(jogador);
-        checaJogador()
-    });
+quadradosVelha.forEach(quadrado => {
+    quadrado.addEventListener('click',clickQuadrados,{once: true}  );
 })
+
+function clickQuadrados (e){
+    let clicandoQuadrado = e.target  
+    clicandoQuadrado.innerHTML = jogador;
+    clicandoQuadrado.style.color = '#F2D338';
+    let qualEVez = jogador === 'X' ? 'O' : 'X'; 
+    mudarJogador(qualEVez);
+    checaJogador()
+}
 
 function mudarJogador(valor){
     jogador = valor
     jogadorSelecionado.innerHTML = 'jogador: ' + jogador;
 }
 
-function checaJogador(){
-    
+function checaJogador(jogadorVencedor){
+    return combinacoesVitorias.some(combinacao =>{
+
+    })
 
     
 }
